4c58bd85a36fb487a84227bd993a8f2dbed10c3e
rssujay <rssujay97@gmail.com>
2022-01-21 20:02:24 +0800

    Initial Commit

---
+++ MVVMTodoList/MVVMTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMTodoListApp.swift
+//  MVVMTodoList
+//
+//  Created by Sujay R Subramanian on 21/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVMTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
